ba4e3c3a04a621709a0a64cb30ea2e55cf72b253
olakt-ogun <79690637+olakt-ogun@users.noreply.github.com>
2023-01-23 20:13:57 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+import HeroScetion from "./components/HeroSection";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container main">
+        <HeroScetion />
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
